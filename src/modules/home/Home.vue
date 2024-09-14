c13d0af4cfd83f4d02a4308c6ed21527bd67b73a
chronosgit <warfacemeister@mail.ru>
2024-09-15 00:09:59 +0500

    improvement: updated the home module

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -1,9 +1,27 @@
 <script setup>
+	import { useTemplateRef, computed, provide } from 'vue';
 	import Header from '../header/Header.vue';
+	import Menu from '../menu/Menu.vue';
+	import useMenu from '@/common/composables/useMenu';
+
+	const menu = useTemplateRef('menu');
+
+	const { isMenuActivated, openMenu } = useMenu(menu);
+
+	provide('openMenu', openMenu);
+
+	const menuComputedStyle = computed(() => ({
+		left: isMenuActivated.value ? 0 : '-100%',
+	}));
 </script>
 
 <template>
 	<div class="home">
+		<!-- toggleable absolute menu -->
+		<div ref="menu" class="menu-wrapper" :style="menuComputedStyle">
+			<Menu />
+		</div>
+
 		<div class="header-wrapper">
 			<Header />
 		</div>
@@ -11,6 +29,16 @@
 </template>
 
 <style scoped>
+	.menu-wrapper {
+		--menu-width: 30%;
+
+		position: absolute;
+		width: var(--menu-width);
+		top: 0;
+		bottom: 0;
+		transition: all 0.5s ease;
+	}
+
 	.home {
 		max-width: 100%;
 		min-height: 100vh;
@@ -24,5 +52,11 @@
 
 	.header-wrapper {
 		padding-top: 0.4rem;
+	}
+
+	@media screen and (min-width: 1280px) {
+		.menu-wrapper {
+			--menu-width: 20%;
+		}
 	}
 </style>
