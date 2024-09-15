01a4ea08fbc71ddb82b89ec14c41817837461c91
chronosgit <warfacemeister@mail.ru>
2024-09-15 18:48:09 +0500

    improvement: updated the filter module

--- src/modules/filter/Filter.vue
+++ src/modules/filter/Filter.vue
@@ -1,3 +1,106 @@
+<script setup>
+	import { computed, useTemplateRef } from 'vue';
+	import IconFilter from '@/common/components/IconFilter.vue';
+	import IconCaretDown from '@/common/components/IconCaretDown.vue';
+	import useClickAway from '@/common/composables/useClickAway';
+
+	const { filters, activeFilter, onFilterChange } = defineProps([
+		'filters',
+		'active-filter',
+		'on-filter-change',
+	]);
+
+	const dropdownAnchor = useTemplateRef('dropdown-anchor');
+
+	const { isElActive: isDropDownOpen, openEl: openDropDown } =
+		useClickAway(dropdownAnchor);
+
+	const dropDownComputedStyle = computed(() => ({
+		maxHeight: isDropDownOpen.value ? '10rem' : 0,
+	}));
+</script>
+
 <template>
-	<section class="filter">Filter</section>
+	<div ref="dropdown-anchor" class="filter-box" @click="openDropDown">
+		<div class="icon-filter-wrapper">
+			<IconFilter />
+		</div>
+
+		<p class="filter-item">{{ activeFilter }}</p>
+
+		<div class="icon-caret-down-wrapper">
+			<IconCaretDown />
+		</div>
+
+		<!-- Absolute toggleable dropdown -->
+		<div class="dropdown" :style="dropDownComputedStyle">
+			<p
+				v-for="(fName, fKey) in filters"
+				class="filter-item dropdown-item"
+				@click="() => onFilterChange(fKey)"
+			>
+				{{ fName }}
+			</p>
+		</div>
+	</div>
 </template>
+
+<style scoped>
+	.filter-box {
+		position: relative;
+		padding: 0.2rem 1rem;
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		background-color: #eefcf7;
+		cursor: pointer;
+	}
+
+	.icon-filter-wrapper {
+		--max-size: 1.8rem;
+		max-width: var(--max-size);
+		max-height: var(--max-size);
+	}
+
+	.filter-item,
+	.dropdown-item {
+		font-size: 2.2rem;
+		font-weight: 900;
+		letter-spacing: 2px;
+		color: #1de390;
+	}
+
+	.filter-item {
+		text-transform: uppercase;
+		margin-right: 0.8rem;
+	}
+
+	.icon-caret-down-wrapper {
+		--max-size: 1.3rem;
+		max-width: var(--max-size);
+		max-height: var(--max-size);
+	}
+
+	.dropdown {
+		position: absolute;
+		top: 0;
+		right: 0;
+		left: 0;
+		transform: translateY(2.8rem);
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: 0.5rem;
+		background-color: #eefcf7;
+		transition: all 0.3s ease;
+		overflow-y: auto;
+	}
+
+	.dropdown::-webkit-scrollbar {
+		width: 0;
+	}
+
+	.dropdown-item {
+		padding-inline: 1rem;
+		cursor: pointer;
+	}
+</style>
