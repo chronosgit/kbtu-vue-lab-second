34e12bbe142715c0b11212bd4024fde58787f820
chronosgit <warfacemeister@mail.ru>
2024-09-15 18:48:25 +0500

    improvement: updated the screen module

---
+++ src/modules/screen/Screen.vue
@@ -0,0 +1,64 @@
+<script setup>
+	import { inject } from 'vue';
+	import Filter from '@modules/filter/Filter.vue';
+	import Date from './components/Date.vue';
+	import Topic from './components/Topic.vue';
+
+	const filtersContext = inject('filtersContext');
+
+	if (!filtersContext) {
+		throw Error(
+			'Screen must consume filtersContext, so it can pass data to Filter component'
+		);
+	}
+
+	const { filters, activeFilter, onFilterChange } = filtersContext;
+</script>
+
+<template>
+	<section class="screen">
+		<main class="main">
+			<div class="date-wrapper">
+				<Date />
+			</div>
+
+			<div class="features">
+				<!-- Make logical -->
+				<Topic :topic="'Adventure'" />
+
+				<Filter
+					:filters
+					:active-filter="activeFilter"
+					@filter-change="onFilterChange"
+				/>
+			</div>
+		</main>
+	</section>
+</template>
+
+<style scoped>
+	.screen {
+		margin: 0 auto;
+		padding: 1rem;
+		width: 90vw;
+		background: linear-gradient(
+			rgba(255, 255, 255, 0.918),
+			rgba(242, 243, 243, 0.658)
+		);
+	}
+
+	.main {
+	}
+
+	.date-wrapper {
+		margin-bottom: 2.5rem;
+		padding: 0.35rem;
+	}
+
+	.features {
+		display: flex;
+		/* justify-content: space-between; */
+		align-items: center;
+		gap: 1rem;
+	}
+</style>
---
+++ src/modules/screen/components/Date.vue
@@ -0,0 +1,31 @@
+<script setup>
+	import { computed } from 'vue';
+	import getReadableDate from '@/common/utils/getReadableDate';
+
+	const now = new Date();
+
+	const readableDate = computed(() => {
+		return getReadableDate(now);
+	});
+</script>
+
+<template>
+	<div class="date-box">
+		<p class="date">{{ readableDate }}</p>
+	</div>
+</template>
+
+<style scoped>
+	.date-box {
+		max-width: max-content;
+		padding-inline: 1rem;
+		background-color: #5bb9cd;
+		box-shadow: 0 0 0.3rem 0.3rem #5bb9cd;
+		border-radius: 0.5rem;
+	}
+
+	.date {
+		font-size: 2.2rem;
+		color: white;
+	}
+</style>
