10412bfd3745f3afe46b2f4243ae606bd8b6916e
chronosgit <warfacemeister@mail.ru>
2024-09-18 22:01:17 +0500

    feat: added arrow img and component

---
+++ src/common/components/IconArrowRight.vue
@@ -0,0 +1,7 @@
+<script setup>
+	import arrowRightIcon from '@assets/images/arrow-right.png';
+</script>
+
+<template>
+	<img :src="arrowRightIcon" alt="arrow is pointing to the right" />
+</template>
