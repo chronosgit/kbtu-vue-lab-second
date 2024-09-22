307e0c8594a42dbd8e60b088f71cb553e0a423c5
chronosgit <warfacemeister@mail.ru>
2024-09-22 22:52:51 +0500

    improvement: injected the pagination functionality into the useUsers hook

--- src/common/composables/useUsers.js
+++ src/common/composables/useUsers.js
@@ -1,23 +1,15 @@
 import { ref, watch } from 'vue';
 import { filter, orderBy } from 'lodash';
+import usePagination from '@common/composables/usePagination';
 import mockUsers from '@common/utils/mockUsers';
 
 const useUsers = (activeCategory, activeFilter) => {
 	// vertical to low pipeline; the lowest one is returned as 'users'
 	const categorizedUsers = ref([]);
 	const filteredUsers = ref([]);
+	const paginatedUsers = ref([]);
 
-	watch(
-		activeCategory,
-		(category) => {
-			const filteredAllUsers = filter(mockUsers, (u) => {
-				return u.topic.toLowerCase() === category.toLowerCase();
-			});
-
-			categorizedUsers.value = filteredAllUsers;
-		},
-		{ immediate: true }
-	);
+	const uPagination = usePagination(filteredUsers);
 
 	const filterByTime = (users) => {
 		const filteredUsers = orderBy(users, [(u) => u.pubData], ['desc']);
@@ -48,9 +40,21 @@ const useUsers = (activeCategory, activeFilter) => {
 	};
 
 	watch(
-		activeFilter,
-		(f) => {
-			switch (f) {
+		activeCategory,
+		(category) => {
+			const filteredAllUsers = filter(mockUsers, (u) => {
+				return u.topic.toLowerCase() === category.toLowerCase();
+			});
+
+			categorizedUsers.value = filteredAllUsers;
+		},
+		{ immediate: true }
+	);
+
+	watch(
+		[activeFilter, categorizedUsers],
+		() => {
+			switch (activeFilter.value) {
 				case 'TIME':
 					const byTime = filterByTime(categorizedUsers.value);
 					filteredUsers.value = byTime;
@@ -68,7 +72,19 @@ const useUsers = (activeCategory, activeFilter) => {
 		{ immediate: true }
 	);
 
-	return { users: filteredUsers, likeUser };
+	watch([uPagination.curPage, filteredUsers], () => {
+		const pagFilUsers = uPagination.getPaginatedItems(filteredUsers.value);
+
+		paginatedUsers.value = pagFilUsers;
+	});
+
+	return {
+		users: paginatedUsers,
+		curPage: uPagination.curPage,
+		totalPages: uPagination.totalPages,
+		likeUser,
+		toNextPage: uPagination.toNextPage,
+	};
 };
 
 export default useUsers;
