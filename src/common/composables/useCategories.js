9863eba176922a5435a2489ac52d20d9f6d88736
chronosgit <warfacemeister@mail.ru>
2024-09-22 20:17:00 +0500

    feat: added common useCategories hook

---
+++ src/common/composables/useCategories.js
@@ -0,0 +1,60 @@
+import { computed, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const useCategories = () => {
+	const router = useRouter();
+	const route = useRoute();
+
+	const activeCategory = ref(null);
+
+	const possibleCategories = {
+		ADVENTURE: 'Adventure',
+		NATURE: 'Nature',
+		FASHION: 'Fashion',
+		MODERN: 'Modern',
+	};
+
+	const categoriesGradients = {
+		ADVENTURE: 'linear-gradient(to right, #4cd87c, #3dd560)',
+		NATURE: 'linear-gradient(to right, #5dde69, #23ce20)',
+		FASHION: 'linear-gradient(to right, #79de56, #49e522)',
+		MODERN: 'linear-gradient(to right, #68e574, #7ecd86)',
+	};
+
+	const isCategoryValid = (category) => {
+		return !Object.keys(possibleCategories).includes(category);
+	};
+
+	const isPathValid = computed(() => {
+		const path = route.fullPath;
+		const pathParts = path.split('/');
+
+		if (!pathParts[1] || !pathParts[2]) return false;
+		if (pathParts[1] !== 'topic') return false;
+		if (!isCategoryValid(pathParts[2])) return false;
+
+		return true;
+	});
+
+	watch(
+		() => route.fullPath,
+		(newPath) => {
+			if (!isPathValid.value) router.push('/not-found');
+
+			const newCategory = newPath.split('/')[2];
+
+			activeCategory.value = newCategory.toUpperCase();
+		},
+		{
+			immediate: true,
+		}
+	);
+
+	return {
+		activeCategory,
+		possibleCategories,
+		categoriesGradients,
+	};
+};
+
+export default useCategories;
